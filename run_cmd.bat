7eb187d5ec279d822803ad7c1a77dffa7e7ddeb3
Tuskul Kaigorodov <kaigorodov.aeb@gmail.com>
2026-08-24 11:33:49 +0900

    Prefer project virtualenv in launcher

--- run_cmd.bat
+++ run_cmd.bat
@@ -1,6 +1,8 @@
 @echo off
 if exist "%~dp0anonymizer.exe" (
   "%~dp0anonymizer.exe" %*
+) else if exist "%~dp0.venv\Scripts\python.exe" (
+  "%~dp0.venv\Scripts\python.exe" "%~dp0anonymize.py" %*
 ) else (
   python "%~dp0anonymize.py" %*
 )
